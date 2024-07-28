dcba455e15ac331e8564617de2b49e0b7bbb1744
ms0242808 <ms0242808@gmail.com>
2024-07-28 10:32:42 +0100

    Updaate UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { fetchUser } from './scripts';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  const getUser = async (search) => {
+    const data = await fetchUser(search);
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser('jane_smith');
+  }, []);
+
+  const onSearch = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    getUser(formData.get('search'));
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <form onSubmit={onSearch}>
+        <input placeholder='search user...' name='search' id='search'/>
+        <button type='submit'>Search</button>
+      </form>
+      
+      {user.map(item => 
+        <div>
+          <p key={item.User_id}>ID: {item.User_id}</p>
+          <p key={item.Email}>Email: {item.Email}</p>
+          <p key={item.Username}>Name: {item.Username}</p>
+        </div>
+      )}
     </div>
   );
 }
